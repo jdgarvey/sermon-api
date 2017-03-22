d8d2a27b5fb276fc9e9d98f267521632346b89af
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-03-22 13:28:14 -0700

    add route for robots.txt

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,5 +2,9 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+  });
   server.use(router);
 };
